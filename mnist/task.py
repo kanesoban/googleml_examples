0a6d18012e9fb4c891301d65d4cda7fc403e8fc4
Zsolnai Csaba <kanesoban@gmail.com>
2020-08-10 09:24:51 +0200

    Fixed cloud GPU training

--- mnist/task.py
+++ mnist/task.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 from .model import Model
 
 
--- train_cloud.sh
+++ train_cloud.sh
@@ -3,17 +3,20 @@
 
 export REGION="europe-west1"
 export BUCKET_NAME="default"
-export GOOGLE_APPLICATION_CREDENTIALS="credentials.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/cszsolnai/Projects/googleml_examples/credentials.json"
 
 
-JOB_NAME="keras_mnist"
-JOB_DIR="gs://$BUCKET_NAME/keras-job-dir"
+JOB_NAME="keras_mnist_${USER}_$(date +%Y%m%d_%H%M%S)"
+export STORAGE_BUCKETNAME=tensorflow-course-256109
+JOB_DIR="gs://$STORAGE_BUCKETNAME/keras-job-dir"
+
 
 gcloud ai-platform jobs submit training $JOB_NAME \
   --package-path mnist/ \
   --module-name mnist.task \
   --region $REGION \
-  --python-version 3.6.7 \
-  --runtime-version 1.15 \
+  --config config.yaml \
+  --python-version 3.7 \
+  --runtime-version 2.1 \
   --job-dir $JOB_DIR \
   --stream-logs
